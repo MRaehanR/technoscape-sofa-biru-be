0e53f1b75754207dc316b009b0f3b53041acea14
MRaehanR <raehanrobban2@gmail.com>
2023-07-29 03:46:48 +0700

    feat(auth): add login service

--- app/Services/Auth/AuthServiceImplement.php
+++ app/Services/Auth/AuthServiceImplement.php
@@ -4,6 +4,8 @@ namespace App\Services\Auth;
 
 use App\Exceptions\ResponseException;
 use App\Models\User;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 
 class AuthServiceImplement implements AuthService
@@ -25,5 +27,22 @@ class AuthServiceImplement implements AuthService
         } catch (\Throwable $th) {
             throw $th;
         }
+    }
+
+    public function login(array $data)
+    {
+        $user = User::where('email', $data['email'])->first();
+
+        if (!$user) {
+            throw new ResponseException('Account not found', Response::HTTP_NOT_FOUND);
+        }
+
+        if (!Auth::attempt(['email' => $data['email'], 'password' => $data['password']])) {
+            throw new ResponseException('Email or Password does not match', Response::HTTP_UNAUTHORIZED);
+        }
+
+        $accessToken = $user->createToken('access_token')->plainTextToken;
+
+        return ['user' => $user, 'access_token' => $accessToken];
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\Auth\RegisterController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -22,3 +23,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 Route::post('/groups', [GroupController::class, 'store']);
 Route::post('/auth/register', RegisterController::class);
+Route::post('/auth/login', LoginController::class);
